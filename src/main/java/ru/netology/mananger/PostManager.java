61be0b8696692eea8c6afd87b4dd31334b1ce9bb
Tatyana Hripkova <tkhripkova@bk.ru>
2020-12-29 23:37:26 +0200

    third commit

---
+++ src/main/java/ru/netology/mananger/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.mananger;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(long ownerId,  boolean ownersOnly, int count, int offset){
+        return null;
+    }
+
+    public void delete(long ownerId, long postId){
+
+    }
+}
